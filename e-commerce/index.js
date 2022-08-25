1dfe63f6011ebb600adad287317398b5f1873a7b
Natallia Bonadia <natallia.bonadia@gmail.com>
2022-08-24 21:41:13 -0300

    Conexão com banco de dados

--- e-commerce/index.js
+++ e-commerce/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
 
 const productsList = [
   {id: 0, name: 'televisao', preco: 3000},
-  {id: 1, name: 'ssd', preco: 1000}
+  {id: 1, name: 'ssd', preco: 1000},
+  {id: 2, name: 'hd', preco: 400}
 ]
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 app.use(express.json())
 
 app.get('/products', (req, res) => {
@@ -14,7 +22,8 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:id', (req, res) => {
   const productId = req.params.id
-  res.json(productsList[productId])
+  const product = productsList.find(e => e.id == productId)
+  res.json(product)
 })
 
 app.post('/products', (req, res) => {
@@ -23,11 +32,27 @@ app.post('/products', (req, res) => {
   res.json(newProduct)
 })
 
-
-
-
-
-app.listen(4000, () => {
-  console.log(`Server running at port 4000`)
+app.delete('/products/:id', (req, res) => {
+  const productId = req.params.id
+  const product = productsList.find(e => e.id == productId)
+  const index = productsList.indexOf(product)
+  productsList.splice(index, 1)
+  res.json(productsList)
 })
+
+app.put('')
+
+
+
+
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v4cwgf8.mongodb.net/bancodedados?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log('Conectamos ao Banco de Dados')
+    app.listen(4000)
+  })
+  .catch(err => console.log(err));
+
+
 
